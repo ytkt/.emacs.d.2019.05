487f3b1e728f07c2d31da104bf14ccd559535dbf
ytkt <yyttkktt@gmail.com>
2016-08-24 02:52:32 +0900

    add ruby-mode hook

--- conf/10-rails.el
+++ conf/10-rails.el
@@ -1,6 +1,8 @@
 (require 'rinari)
 
 (require 'rhtml-mode)
+(require 'slim-mode)
+
 (add-hook 'rhtml-mode-hook
           (lambda () (rinari-launch)))
 
@@ -11,3 +13,26 @@
 
 ;; yasnippets-rails
 (yas/load-directory "~/.emacs.d/snippets/yasnippets-rails")
+
+
+(defun ruby-mode-hook ()
+  (autoload 'ruby-mode "ruby-mode" nil t)
+  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Bowerfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+  (add-hook 'ruby-mode-hook '(lambda ()
+                               (setq ruby-deep-arglist t)
+                               (setq ruby-deep-indent-paren nil)
+                               (setq c-tab-always-indent nil)
+                               (require 'inf-ruby)
+                               (require 'ruby-compilation))))
+
+(require 'rspec-mode)
+(custom-set-variables '(rspec-use-rake-flag nil))
